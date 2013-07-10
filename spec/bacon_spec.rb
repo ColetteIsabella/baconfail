489fbb3e888568d3583aa274d653fb7a41c8b38a
Colette Taylor <coletteisabella@gmail.com>
2013-07-09 21:42:54 -0700

    Inedible bacon
    It is what it is.

---
+++ spec/bacon_spec.rb
@@ -0,0 +1,18 @@
+require 'bacon'
+
+describe Bacon do 
+	subject { Bacon.new }
+
+	its (:edible?) { should be_true }
+
+
+	it "expired!" do
+		bacon = Bacon.new
+		bacon.expired!
+		bacon.expired.should be_true
+	end
+it "expired!" do
+	subject.expired!
+	subject.should_not be_edible
+	
+end
